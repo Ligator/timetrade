552fe010a75a9e3823e97b941dd4c409be32ccb1
AlfredoJA <alfre.jangeles@hotmail.com>
2023-05-19 02:01:44 -0600

    Added users relation

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,2 +1,4 @@
 class Ranking < ApplicationRecord
+  belongs_to :evaluator, class_name: "User", foreign_key: "evaluator_id"
+  belongs_to :evaluated, class_name: "User", foreign_key: "evaluated_id"
 end
