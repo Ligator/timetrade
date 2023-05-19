58b6732d77f22eef746a707e60f5e969a9c3b820
davepcage <davidpachecocontacto@gmail.com>
2023-05-18 22:13:12 -0600

    database created

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_031203) do
-  create_table "models", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_05_19_041220) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -24,8 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_031203) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
