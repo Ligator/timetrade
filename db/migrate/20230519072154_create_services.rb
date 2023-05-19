079ddd25f84464afda13fd1581ad59f713ebcec0
davepcage <davidpachecocontacto@gmail.com>
2023-05-19 01:22:32 -0600

    scafold service

---
+++ db/migrate/20230519072154_create_services.rb
@@ -0,0 +1,12 @@
+class CreateServices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :services do |t|
+      t.string :description
+      t.bigint :beneficiary_id
+      t.bigint :supplier_id
+      t.bigint :task_id
+
+      t.timestamps
+    end
+  end
+end
