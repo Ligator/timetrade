ac7931df5e38366dbe3992512cb95f7eec079530
davepcage <davidpachecocontacto@gmail.com>
2023-05-19 14:29:42 -0600

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,4 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :firstname, :lastname, :phone, :birthday, 
-                                 :gender, :requested_services_id, :offered_services)
-  end
-
-
 end
