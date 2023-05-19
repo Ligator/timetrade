62f8e21d0b83ce1e93a94901ac4b30ee52ef2cf1
Abel Pacheco <abelpacheco@outlook.com>
2023-05-19 03:08:34 -0600

    Set root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :tasks
   resources :services
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "services#index"
 end
