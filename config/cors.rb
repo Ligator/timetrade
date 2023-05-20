5e58c2b125291a665055541f1606bc5a5ccff2d8
Abel Pacheco <abelpacheco@outlook.com>
2023-05-19 18:51:35 -0600

    Update cors.rb

--- config/cors.rb
+++ config/cors.rb
@@ -1,17 +1,25 @@
-use Rack::Cors do
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :delete, :put, :options]
+    resource '*', headers: :any, methods: [:get, :post]
   end
-
-  # allow do
-  #   origins '*'
-  #   resource '/public/*', headers: :any, methods: :get
-
-  #   # Only allow a request for a specific host
-  #   resource '/api/v1/*',
-  #       headers: :any,
-  #       methods: :get,
-  #       if: proc { |env| env['HTTP_HOST'] == 'api.example.com' }
-  # end
 end
+
+# use Rack::Cors do
+#   allow do
+#     origins '*'
+#     resource '*', headers: :any, methods: [:get, :post, :delete, :put, :options]
+#   end
+
+#   # allow do
+#   #   origins '*'
+#   #   resource '/public/*', headers: :any, methods: :get
+
+#   #   # Only allow a request for a specific host
+#   #   resource '/api/v1/*',
+#   #       headers: :any,
+#   #       methods: :get,
+#   #       if: proc { |env| env['HTTP_HOST'] == 'api.example.com' }
+#   # end
+# end
