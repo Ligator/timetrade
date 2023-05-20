a865b720df068260ae0d92842b7522f1a8e330d3
Abel Pacheco <abelpacheco@outlook.com>
2023-05-20 17:48:18 -0600

    CORS

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module Timetrade
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://timetrade.vercel.app'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+        origins 'timetrade.vercel.app'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :head, :options]
       end
     end
   end
