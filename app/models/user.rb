18a157041fe46318c16bc7c2ab4a00b79ab97c3b
AlfredoJA <alfre.jangeles@hotmail.com>
2023-05-19 19:05:39 -0600

    Added user insights

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,25 @@ class User < ApplicationRecord
   has_many :requested_tasks, class_name: "Task", foreign_key: "beneficiary_id"
   has_many :requested_services, class_name: "Service", foreign_key: "beneficiary_id"
   has_many :offered_services, class_name: "Service", foreign_key: "supplier_id"
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :birthday, presence: true
+  validates :gender, presence: true
+
+  def average_score
+    rankings.sum(&:score) / rankings.count
+  end
+
+  def offered_hours
+    offered_tasks.sum(&:time)
+  end
+
+  def requested_hours
+    requested_tasks.sum(&:time)
+  end
+
+  def hours_balance
+    offered_hours - requested_hours
+  end
 end
