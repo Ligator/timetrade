773c5fe5d74d6a7ec6a47b92903b93b3752c692d
Abel Pacheco <abelpacheco@outlook.com>
2023-05-19 18:55:30 -0600

    Update cors.rb

--- config/cors.rb
+++ config/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://timetrade-qig6i14ff-juanmavelez.vercel.app'
     resource '*', headers: :any, methods: [:get, :post]
   end
 end
