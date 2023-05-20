12fe35a2c19873215d42883921c1766464db3a1d
Abel Pacheco <abelpacheco@outlook.com>
2023-05-19 22:08:30 -0600

    User can storage extra parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 	before_action :authenticate_user!
 	skip_before_action :verify_authenticity_token
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :birthday, :gender, :phone])
+  end
 end
