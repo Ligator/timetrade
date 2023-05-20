893a7928c82f585880859f96eb7cea6a9342682b
AlfredoJA <alfre.jangeles@hotmail.com>
2023-05-19 18:23:33 -0600

    Added model validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,9 @@ class Task < ApplicationRecord
   belongs_to :beneficiary, class_name: "User", foreign_key: "beneficiary_id"
   belongs_to :supplier, class_name: "User", foreign_key: "supplier_id"
   belongs_to :service
+
   has_one :ranking
+
+  validates :time, numericality: { only_integer: true, greater_than: 0 }
+  validates :description, presence: true
 end
