445e2432e43ae40e3ea05b93411eb3c3e65bc905
AlfredoJA <alfre.jangeles@hotmail.com>
2023-05-20 21:47:56 -0600

    Update index boostrap

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,7 +4,8 @@ class ServicesController < ApplicationController
   # GET /services or /services.json
   def index
     @services = Service.all
-    @group_services = @services.shuffle.in_groups_of(5, false).slice(0, 3)
+    @group_services = @services.shuffle.in_groups_of(15, false).slice(0, 3)
+    @group_names = ["Hoy", "Recientemente Agregados", "Mas Antiguos"]
   end
 
   # GET /services/1 or /services/1.json
